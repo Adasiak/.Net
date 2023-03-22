c6c4779cf4e97d5c0d3583ed04dde8d0ef2eb0eb
Luminaren330 <259256@student.pwr.edu.pl>
2023-03-22 15:35:20 +0100

    test

---
+++ Backpack/Item.cs
@@ -0,0 +1,32 @@
+﻿using System.Runtime.InteropServices;
+using System;
+
+namespace Backpack
+{
+    public class Item
+    {
+        int value;
+        int weight;
+        double ratio;
+        public Item(int value, int weight)
+        {
+            this.value = value;
+            this.weight = weight;
+            this.ratio = Decimal.ToDouble(value) / Decimal.ToDouble(weight);
+
+        }
+        public double GetRatio()
+        {
+            return this.ratio;
+        }
+        public int GetWeight()
+        {
+            return this.weight;
+        }
+        public int GetValue()
+        {
+            return this.value;
+        }
+    }
+
+}
