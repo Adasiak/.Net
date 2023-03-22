9a06ac83dcbd0b154d153509f25c7ceb47499337
Luminaren330 <259256@student.pwr.edu.pl>
2023-03-22 17:19:52 +0100

    Aplikacja okienkowa zrobiona

--- WindowForm/WindowForm/WindowForm/BackpackForm.cs
+++ WindowForm/WindowForm/WindowForm/BackpackForm.cs
@@ -16,5 +16,35 @@ namespace WindowForm
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int k = int.Parse(textBox2.Text);
+            int capacity = int.Parse(textBox1.Text);
+            Random rnd = new Random();
+            List<Item> list = new List<Item>();
+            for (int j = 0; j < k; j++)
+            {
+                list.Add(new Item(j, rnd.Next(1, 40), rnd.Next(1, 40)));
+            }
+            foreach (Item item in list)
+            {
+                listBox1.Items.Add("Item " + item.GetIndex() + ": Weight " + item.GetWeight() + " Value " + item.GetValue() + " Ratio " + item.GetRatio());
+            }
+            List<Item> sortedList = list.OrderByDescending(o => o.GetRatio()).ToList();
+            foreach (Item item in sortedList)
+            {
+                listBox2.Items.Add("Item " + item.GetIndex() + ": Weight " + item.GetWeight() + " Value " + item.GetValue() + " Ratio " + item.GetRatio());
+            }
+            Backpack1 backpack = new Backpack1(capacity);
+            backpack.getElement(sortedList);
+
+            foreach (Item item in backpack.getItemsinBackpack())
+            {
+                listBox3.Items.Add("Item " + item.GetIndex() + " Weight  " + item.GetWeight() + " Value " + item.GetValue());
+
+            }
+            listBox4.Items.Add("Total weight: " + backpack.getTotalWeight() + " Total value: " + backpack.getTotalValue());
+        }
     }
 }
--- WindowForm/WindowForm/WindowForm/Program.cs
+++ WindowForm/WindowForm/WindowForm/Program.cs
@@ -12,40 +12,6 @@ namespace WindowForm
         static void Main(string[] args)
         {
             Application.Run(new BackpackForm());
-            int k = 15;
-            int capacity = 60;
-            Random rnd = new Random();
-            List<Item> list = new List<Item>();
-            for (int j = 0; j < k; j++)
-            {
-                list.Add(new Item(j, rnd.Next(1, 40), rnd.Next(1, 40)));
-            }
-            Console.WriteLine("Normal List: ");
-            foreach (Item item in list)
-            {
-                Console.WriteLine("Item " + item.GetIndex() + ": Weight " + item.GetWeight() + " Value " + item.GetValue() + " Ratio " + item.GetRatio());
-            }
-            Console.WriteLine("---------------------------------");
-            Console.WriteLine("Sorted List:");
-
-            List<Item> sortedList = list.OrderByDescending(o => o.GetRatio()).ToList();
-            foreach (Item item in sortedList)
-            {
-                Console.WriteLine("Item " + item.GetIndex() + ": Weight " + item.GetWeight() + " Value " + item.GetValue() + " Ratio " + item.GetRatio());
-            }
-            Console.WriteLine("---------------------------------");
-            Console.WriteLine("Elements in Backpack of capacity " + capacity + ": ");
-            Backpack1 backpack = new Backpack1(capacity);
-            backpack.getElement(sortedList);
-
-            foreach (Item item in backpack.getItemsinBackpack())
-            {
-                Console.WriteLine("Item " + item.GetIndex() + " Weight  " + item.GetWeight() + " Value " + item.GetValue());
-
-            }
-            Console.WriteLine("Total weight: " + backpack.getTotalWeight() + " Total value: " + backpack.getTotalValue());
-
-            System.Console.Read();
         }
     }
 }
