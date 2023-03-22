95150fab0a606085cff22a8f6d34fac3a810607d
Luminaren330 <259256@student.pwr.edu.pl>
2023-03-22 16:45:23 +0100

    Added window form

---
+++ WindowForm/WindowForm/WindowForm/Backpack1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowForm
+{
+    public class Backpack1
+    {
+        int capacity;
+        int total_weight;
+        int total_value;
+        List<Item> itemsInBackPack;
+        public Backpack1(int capacity)
+        {
+            this.capacity = capacity;
+            this.itemsInBackPack = new List<Item>();
+            this.total_weight = 0;
+            this.total_value = 0;
+        }
+        public void getElement(List<Item> items)
+        {
+            foreach (Item item in items)
+            {
+                if (item.GetWeight() < capacity)
+                {
+                    this.itemsInBackPack.Add(item);
+                    this.capacity -= item.GetWeight();
+                    this.total_weight += item.GetWeight();
+                    this.total_value += item.GetValue();
+                }
+            }
+
+        }
+        public List<Item> getItemsinBackpack()
+        {
+            return this.itemsInBackPack;
+        }
+        public int getTotalWeight()
+        {
+            return this.total_weight;
+        }
+        public int getTotalValue()
+        {
+            return this.total_value;
+        }
+    }
+}
---
+++ WindowForm/WindowForm/WindowForm/Item.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowForm
+{
+    public class Item
+    {
+        int item_index;
+        int value;
+        int weight;
+        double ratio;
+        public Item(int item_index, int value, int weight)
+        {
+            this.item_index = item_index;
+            this.value = value;
+            this.weight = weight;
+            this.ratio = Decimal.ToDouble(value) / Decimal.ToDouble(weight);
+
+        }
+        public double GetRatio()
+        {
+            return this.ratio;
+        }
+        public int GetWeight()
+        {
+            return this.weight;
+        }
+        public int GetValue()
+        {
+            return this.value;
+        }
+        public int GetIndex()
+        {
+            return this.item_index;
+        }
+    }
+}
