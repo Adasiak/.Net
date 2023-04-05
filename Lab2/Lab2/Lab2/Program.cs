71782d9525fc0bdf3ff96fc4b24ec70448c24769
Luminaren330 <259256@student.pwr.edu.pl>
2023-04-05 09:11:34 +0200

    Rozpoczęcie projektu 2

---
+++ Lab2/Lab2/Lab2/Program.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string call = "http://radoslaw.idzikowski.staff.iiar.pwr.wroc.pl/instruction/students.json";
+            HttpClient client = new HttpClient();
+            string response = await client.GetStringAsync(call);
+            List<Student> students = JsonConvert.DeserializeObject<List<Student>>(response);
+            foreach (var s in students)
+                Console.WriteLine(s.studentId + ":\t" + s.studentName);
+            Console.Read();
+        }
+        
+    }
+}
