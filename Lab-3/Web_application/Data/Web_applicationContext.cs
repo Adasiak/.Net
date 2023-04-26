16e126e60046537146002d117e2f17e02c45ae90
aaaa <258974@student.pwr.edu.pl>
2023-04-26 19:15:55 +0200

    Jest piwo

---
+++ Lab-3/Web_application/Data/Web_applicationContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Web_application.models;
+
+namespace Web_application.Data
+{
+    public class Web_applicationContext : DbContext
+    {
+        public Web_applicationContext (DbContextOptions<Web_applicationContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Web_application.models.Beer> Beer { get; set; } = default!;
+    }
+}
---
+++ Lab-3/Web_application/models/Beer.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web_application.models
+{
+    public class Beer
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public float Voltage { get; set; }
+        public float Volume { get; set; }
+        public float Price { get; set; }
+        [DataType(DataType.Date)]
+
+        public DateTime Deadline { get; set; }
+
+    }
+}
